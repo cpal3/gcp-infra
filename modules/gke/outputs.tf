7f72865b1dcdd285daa52e2f69ef5ddfccbb5dcc
cpal3 <chandramani.pal@gmail.com>
2026-03-29 18:12:02 +0530

    enable gke fleet

--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -1,7 +1,7 @@
-output "cluster_name" {
-  description = "The name of the GKE cluster."
-  value       = google_container_cluster.primary.name
-}
+#output "cluster_name" {
+#  description = "The name of the GKE cluster."
+#  value       = google_container_cluster.primary.name
+#}
 
 output "cluster_endpoint" {
   description = "The IP address of the cluster master."
